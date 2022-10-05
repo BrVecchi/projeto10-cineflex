6e6cded1a23e2bf18e581d744abe6f7e820a12d6
BrVecchi <bno.vecchi@gmail.com>
2022-10-05 12:31:57 -0300

    projeto iniciado com as bibliotecas instaladas

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import GlobalStyle from "./GlobalStyle";
+import ResetCSS from "./ResetCSS";
+
+export default function App() {
+    return (
+        <div>
+        <ResetCSS/>
+        <GlobalStyle/>
+        <GlobalStyle />
+        <div>Hello!</div>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// font-family: 'Roboto', sans-serif;
+
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.querySelector('.root'));
